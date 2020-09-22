050738e1cbac38467845ec770ca379c68b743655
CobuAdam <71340963+CobuAdam@users.noreply.github.com>
2020-09-22 15:21:06 -0700

    Add files via upload

---
+++ adamsweirdchat.py
@@ -0,0 +1,73 @@
+print ("rice")
+print("panda")
+print("what is 505 +584")
+print(505+584)
+print("505+584=1089")
+a="milk"
+b="water"
+#c=b
+#b=a
+#a=c
+print(a)
+print(b)
+h1="dragon"
+h2="person"
+h3=h1
+h1=h3
+h3=h2
+print(h1)
+print(h3)
+
+c=a
+d=b
+b=c
+a=d
+print(a)
+print(b)
+
+my_pic='''
+         ttttttt
+         666666
+         uuuuu'''
+print(my_pic)
+
+my_pic='''
+[_____________]
+!!!!!!!!!!!!!!!'''
+
+
+
+        '''
+        +----+
+              |
+              |
+              |
+              |
+            =====
+         '''
+
+        '''
+        +----+
+        0     |
+              |
+              |
+              |
+            =====
+         '''
+        '''+----+
+        0     |
+        |     |
+              |
+              |
+            ====='''
+
+        '''+----+
+        0     |
+        |     |
+              |
+              |
+            ====='''
+
+
+
+
